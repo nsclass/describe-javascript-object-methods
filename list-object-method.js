b093030c914fe511e412277127f02dbc3cb41325
Nam Seob Seo <nsclass@gmail.com>
2019-10-12 20:39:05 -0500

    describing all methods and properties in a Javascript object.

---
+++ list-object-method.js
@@ -0,0 +1,33 @@
+const getArgs = func => {
+  // First match everything inside the function argument parens.
+  let matchedItems = func.toString().match(/function\s.*?\(([^)]*)\)/)
+  if (matchedItems && matchedItems.length > 0) {
+    let args = matchedItems[1].trim()
+    if (args.length > 0) {
+ 
+      // Split the arguments string into an array comma delimited.
+      return args.split(',').map( arg => {
+        // Ensure no inline comments are parsed and trim the whitespace.
+        return arg.replace(/\/\*.*\*\//, '').trim()
+      })
+    }
+  }
+
+  return "no args"
+}
+
+export const getMethods = (obj) => {
+  return Object.entries(obj)
+  .filter(([key, value]) => typeof obj[key] === 'function')
+  .map(([key, value]) => `${key}: ${getArgs(value)}`)
+  .sort((x, y) => {
+    let strX = x.toString()
+    let strY = y.toString()
+    return strX.localeCompare(strY)
+  })
+}
+
+export const getProperties = obj => {
+  return Object.getOwnPropertyNames(obj)
+  .filter(item => typeof obj[item] !== 'function')
+}
